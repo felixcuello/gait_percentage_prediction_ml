f9223b4163de7d6410143109c905728a483502cb
Nicolas Ponce <nicohilton@live.com.ar>
2023-03-29 16:02:28 -0300

    CSV to Matrix Converter
     Python script converts a CSV file into a matrix

---
+++ csv_to_matrix.py
@@ -0,0 +1,26 @@
+#!/Users/nico/Desktop/GaitUP - UP/np.py
+import numpy as np
+
+# carga data
+
+data = np.genfromtxt('ShaAng_W50.csv', delimiter=',')[1:]
+matrix = np.zeros((2100, 2), dtype=float) 
+
+# Iteración 
+row = 0
+col = 0
+for i in range(2100):
+    matrix[i,0] = int(row + 1)
+    matrix[i,1] = float(data[row, col])
+    
+    row += 1
+    if row % 100 == 0:
+        col += 1
+        row = 0
+
+# seteo decimales para revisar
+#np.set_printoptions(precision=15)
+
+#np.savetxt('matrix.txt', matrix, fmt='%.15f')
+
+#print(matrix[:200])

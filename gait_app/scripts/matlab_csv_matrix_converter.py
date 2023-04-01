9f56bd987c422b7e1c3a1961a220c4a6cdec2217
Nicolas Ponce <nicohilton@live.com.ar>
2023-04-01 13:18:48 -0300

    matlab_csv_matrix_converter.py
    Combination of both Mat2csv.py and csv_to_matrix.py

---
+++ gait_app/scripts/matlab_csv_matrix_converter.py
@@ -0,0 +1,56 @@
+# Libraries
+import os
+import re
+import csv
+import sys
+import logging
+import numpy as np
+from scipy.io import loadmat
+
+# Setting log level
+os_log_level = os.environ['LOG_LEVEL'] if 'LOG_LEVEL' in os.environ else 'INFO'
+logging.root.setLevel(os_log_level)
+
+# Checking parameters
+if len(sys.argv) != 3:
+    print("Usage {} <mat_file> <output_matrix_file>".format(sys.argv[0]))
+    sys.exit()
+
+# Setting the matlab variables
+mat_file = sys.argv[1]
+mat_data = loadmat(mat_file)
+
+# CSV TEMPORAL
+csv_filename = sys.argv[2]
+
+# Checking matlab columns
+for col in mat_data.keys():
+    # Only process the non administrative column (non __)
+    if not re.search("__", col):
+        with open(csv_filename, 'w', newline='') as csv_file:
+            logging.info('Processing {} converting into {}'.format(mat_file, csv_filename))
+            writer = csv.writer(csv_file)
+            subjects = []
+            for subject_id in range(0,21):
+                subjects.append("subject_{}".format(subject_id))
+            writer.writerow(subjects)
+            for row in mat_data[col]:
+                writer.writerow(row)
+
+# Load data from CSV
+data = np.genfromtxt(csv_filename, delimiter=',')[1:]
+matrix = np.zeros((2100, 2), dtype=float)           #Matriz relación Gait - Angle de cada muestra
+
+# Iteración 
+row = 0
+col = 0
+for i in range(2100):
+    matrix[i,0] = int(row + 1)
+    matrix[i,1] = float(data[row, col])
+    
+    row += 1
+    if row % 100 == 0:
+        col += 1
+        row = 0
+
+os.remove(csv_filename)  #borro csv temporal

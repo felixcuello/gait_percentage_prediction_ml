dd424674f19496b564b4827d90af3475e046036d
Nicolas Ponce <nicohilton@live.com.ar>
2023-04-21 13:58:08 -0300

    Update TestGPE.py
    updated comments

--- gait_app/scripts/TestGPE.py
+++ gait_app/scripts/TestGPE.py
@@ -4,11 +4,11 @@ import pandas as pd
 
 # Example input values
 new_data = pd.DataFrame({
-    'ShankAngles': [123], # Replace 30 with the desired angle value
-    'ShankVelocity': [0.5], # Replace 0.5 with the corresponding velocity value
-    'ShankAngularVelocity': [np.nan], # Replace 2 with the corresponding angular velocity value
+    'ShankAngles': [], # Replace with the desired angle value
+    'ShankVelocity': [], # Replace with the corresponding velocity value
+    'ShankAngularVelocity': [np.nan], # Replace with the corresponding angular velocity value
     'ShankIntegral': [np.nan], # This value is not used in prediction and can be set to 0
-    'ThighAngles': [np.nan], # This value is not used in prediction and can be set to 0
+    'ThighAngles': [np.nan], # Replace with the desired angle value
     'ThighVelocity': [np.nan], # This value is not used in prediction and can be set to 0
     'ThighAngularVelocity': [np.nan], # This value is not used in prediction and can be set to 0
     'ThighIntegral': [np.nan] # This value is not used in prediction and can be set to 0
